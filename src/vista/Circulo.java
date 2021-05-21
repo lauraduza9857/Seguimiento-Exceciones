59098e0df6021879bd4d730df02e4ca0e0477787
iisa-gallego <isabella.gallego2@correo.icesi.edu.co>
2021-05-20 20:00:54 -0500

    creé graficamente los botones

--- src/vista/Circulo.java
+++ src/vista/Circulo.java
@@ -6,18 +6,15 @@ public class Circulo extends Figura{
 
 	public Circulo(int posX, int posY, int width, int height, PApplet app) {
 		super(posX, posY, width, height, app);
-		// TODO Auto-generated constructor stub
+	
 	}
 
-	@Override
 	public void pintar() {
 		try {
 			this.app.fill(r,g,b);
 		} catch (Exception e) {
-			// TODO: handle exception
 			System.out.println(e.getMessage());
 		}
-		// TODO Auto-generated method stub
 		
 		this.app.strokeWeight(1);
 		this.app.stroke(0);
@@ -27,7 +24,6 @@ public class Circulo extends Figura{
 		}
 		
 		this.app.ellipse(posX, posY, width,height);
-		
 		
 	}
 
--- src/vista/Cuadrado.java
+++ src/vista/Cuadrado.java
@@ -8,22 +8,16 @@ public class Cuadrado extends Figura{
 
 	public Cuadrado(int posX, int posY, int width, int height, PApplet app) {
 		super(posX, posY, width, height, app);
-		// TODO Auto-generated constructor stub
-		
-		
 	}
 	
 	public Cuadrado(int posX, int posY, int width, int height, PApplet app, int r, int g, int b) {
 		super(posX, posY, width, height, app, r, g ,b);
 	}
 
-	@Override
 	public void pintar() {
-		// TODO Auto-generated method stub
 		try {
 			this.app.fill(r,g,b);
 		} catch (Exception e) {
-			// TODO: handle exception
 			System.out.println(e.getMessage());
 		}
 		this.app.strokeWeight(1);
